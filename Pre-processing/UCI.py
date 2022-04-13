3f8ea401851d0b284abe51a0a88531b68405f25e
Xiaer <67382837+Chaolei98@users.noreply.github.com>
2022-04-13 23:15:50 +0800

    Add files via upload

---
+++ Pre-processing/UCI.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+
+def make_x(path):
+    file_path = path
+    all_file_names = os.listdir(file_path)
+    x = []
+    for file_name in all_file_names:
+        x.append(np.genfromtxt(path + '/' + file_name))
+    return np.transpose(x, (1, 2, 0))
+
+train_x = make_x('./train/Inertial Signals')
+test_x = make_x('./test/Inertial Signals')
+train_y = np.genfromtxt('./train/y_train.txt').reshape(-1)
+test_y = np.genfromtxt('./test/y_test.txt').reshape(-1)
