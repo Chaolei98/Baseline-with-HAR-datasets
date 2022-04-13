8a139961270246046b0df4ecf13b1dea10ab2752
Xiaer <67382837+Chaolei98@users.noreply.github.com>
2022-04-13 20:07:05 +0800

    Update OPP_ResNet.py

--- OPPORTUNITY/OPP_ResNet.py
+++ OPPORTUNITY/OPP_ResNet.py
@@ -17,14 +17,10 @@ parser.add_argument('--epoch', type=int, default=200, help='Epoch')
 args = parser.parse_args()
 
 #load data
-# train_x = torch.from_numpy(np.load('/data1/experiment/hcl/Datasets/opportunity/x_train.npy')).float()
-# train_y = torch.from_numpy(np.load('/data1/experiment/hcl/Datasets/opportunity/y_train.npy')).long()
-# test_x = torch.from_numpy(np.load('/data1/experiment/hcl/Datasets/opportunity/x_test.npy')).float()
-# test_y = torch.from_numpy(np.load('/data1/experiment/hcl/Datasets/opportunity/y_test.npy')).long()
-train_x = torch.from_numpy(np.load('/mnt/experiment/hcl/Datasets/opportunity/x_train.npy')).float()
-train_y = torch.from_numpy(np.load('/mnt/experiment/hcl/Datasets/opportunity/y_train.npy')).long()
-test_x = torch.from_numpy(np.load('/mnt/experiment/hcl/Datasets/opportunity/x_test.npy')).float()
-test_y = torch.from_numpy(np.load('/mnt/experiment/hcl/Datasets/opportunity/y_test.npy')).long()
+train_x = torch.from_numpy(np.load('/Datasets/opportunity/x_train.npy')).float()
+train_y = torch.from_numpy(np.load('/Datasets/opportunity/y_train.npy')).long()
+test_x = torch.from_numpy(np.load('/Datasets/opportunity/x_test.npy')).float()
+test_y = torch.from_numpy(np.load('/Datasets/opportunity/y_test.npy')).long()
 
 train_x = torch.unsqueeze(train_x, 1)
 test_x = torch.unsqueeze(test_x, 1)
